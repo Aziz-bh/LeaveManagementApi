5405ca464197d61caea9c9fc7cf7f59a72db72b8
Aziz benhmida <98248923+Aziz-bh@users.noreply.github.com>
2025-04-16 22:08:03 +0100

    feat(dto): add LeaveRequestFilterDto for advanced leave filtering

---
+++ Application/DTOs/LeaveRequestFilterDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs;
+
+public class LeaveRequestFilterDto
+{
+    public int? EmployeeId { get; set; }
+    public LeaveType? LeaveType { get; set; }
+    public LeaveStatus? Status { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string? Keyword { get; set; }
+    //those properties are used for pagination and sorting
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string SortBy { get; set; } = "CreatedAt";
+    public string SortOrder { get; set; } = "desc";
+}
