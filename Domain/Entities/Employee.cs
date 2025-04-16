eccb6b010021c64446a4a941bf5d370d6f9d66a1
Aziz benhmida <98248923+Aziz-bh@users.noreply.github.com>
2025-04-16 13:19:11 +0100

    Add Domain layer with core entities and enums
    - Created Domain class library for clean architecture
    - Added Employee and LeaveRequest entities with navigation properties
    - Defined LeaveType and LeaveStatus enums for type safety
    - Added Domain project to solution

---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public ICollection<LeaveRequest> LeaveRequests { get; set; }
+    }
+}
---
+++ Domain/Enums/LeaveStatus.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums;
+public enum LeaveStatus { Pending, Approved, Rejected }
